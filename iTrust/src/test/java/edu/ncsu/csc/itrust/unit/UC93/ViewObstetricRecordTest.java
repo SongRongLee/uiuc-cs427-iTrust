e0039a2ba75b8f68c53897875d9e716bcb79e2b5
julia0607 <40482344+julia0607@users.noreply.github.com>
2018-10-26 19:43:49 -0500

    create ViewObstetricRecordTest.java

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/UC93/ViewObstetricRecordTest.java
@@ -0,0 +1,20 @@
+package edu.ncsu.csc.itrust.unit.UC93;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
+
+public class ViewObstetricRecordTest {
+
+	WebConversation wc = login("9000000003","pw");
+	WebResponse wr = login("9000000003","pw");
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
