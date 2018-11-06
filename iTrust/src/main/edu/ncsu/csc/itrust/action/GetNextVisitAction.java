7c6d959b5191d31606687a5b4c32121f9c723350
ccxu2 <ccxu2@illinois.edu>
2018-11-06 17:34:08 -0600

    Google Calendar Integration Completed.

--- iTrust/src/main/edu/ncsu/csc/itrust/action/GetNextVisitAction.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/action/GetNextVisitAction.java
@@ -1,8 +1,19 @@
 package edu.ncsu.csc.itrust.action;
 
 import edu.ncsu.csc.itrust.model.old.beans.ObstetricsVisitBean;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.*;
+
 import java.text.SimpleDateFormat;
 
 /**
@@ -33,9 +44,94 @@ public class GetNextVisitAction {
 			
 		}
 		
+
+		
+		
 		CheckAndUpdate(cal);
+		Timestamp defaultTime = new Timestamp(cal.getTime().getTime());
+		
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		Timestamp startTime = new Timestamp(cal.getTime().getTime());
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		Timestamp endTime = new Timestamp(cal.getTime().getTime());
+		List<Timestamp>listSchedule = new ArrayList<Timestamp>();
+		try {
+			listSchedule = getSchedule(startTime,endTime);
+		}catch(Exception e) {
+			
+		}
+		defaultTime = GetNextTime(listSchedule, defaultTime);
+		
+		
+		
 		SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyyy HH:mm");
-		return f.format(cal.getTime().getTime());
+		return f.format(defaultTime);
+	}
+	
+	/**
+	 * 
+	 * Approximately calculate next visit time
+	 */
+	public Timestamp GetNextTime(List<Timestamp> et, Timestamp dt) {
+	
+		// define the appointment period and increment period
+		int app_slot = 1800;
+		int t_inc = 1800;
+		
+		Timestamp st_s = dt;
+		Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(dt.getTime());
+        cal.add(Calendar.SECOND, app_slot);
+        Timestamp st_e = new Timestamp(cal.getTime().getTime());
+        
+        // Verify if the patient is available during the default time
+        
+        int List_len = et.size();
+        int i = 0;
+        boolean available = true;
+        while (i<List_len) {
+            available = available && (st_e.before(et.get(i)) || st_s.after(et.get(i+1)));
+            i = i + 2;
+        }
+        if(available) {
+        	return st_s;
+        }
+		
+        // Loop through the day to find the earliest availability
+        // Set last time slot
+		cal.set(Calendar.HOUR_OF_DAY, 16);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.add(Calendar.SECOND, - app_slot);
+        Timestamp st_lim = new Timestamp(cal.getTime().getTime());
+        // Initialize the start time
+		cal.set(Calendar.HOUR_OF_DAY, 8);
+		cal.set(Calendar.MINUTE, 30);
+		cal.set(Calendar.SECOND, 0);
+        st_s = new Timestamp(cal.getTime().getTime());
+        cal.add(Calendar.SECOND, app_slot);
+        // Increment time
+        while(st_s.before(st_lim)) {
+            st_s = new Timestamp(cal.getTime().getTime());
+            cal.add(Calendar.SECOND, app_slot);
+            st_e = new Timestamp(cal.getTime().getTime());
+        	// Loop through each event and check for the availability
+            i = 0;
+            available =  true;
+            while (i<List_len) {
+                available = available && (st_e.before(et.get(i)) || st_s.after(et.get(i+1)));
+                i = i + 2;
+            }
+            if(available) {
+            	return st_s;
+            }
+            cal.add(Calendar.SECOND, t_inc - app_slot);
+        }
+		return st_s;
 	}
 	
 	
@@ -125,4 +221,60 @@ public class GetNextVisitAction {
 		// IF NOTHING ELSE, IT'S A BUSINESS DAY
 		return true;
 	}
+	
+	
+	public String TimestampToRFC3399(Timestamp ts){
+		Date d = new Date(ts.getTime());
+		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(d);
+	}
+	
+	public Timestamp RFC3399ToTimestamp(String rts){
+		Timestamp ts = null;
+		try{
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+			Date parsedDate = dateFormat.parse(rts);
+		    ts = new java.sql.Timestamp(parsedDate.getTime());
+		}catch(Exception e){
+		}
+		return ts;
+	}
+	
+	public List<Timestamp> getSchedule(Timestamp tMin, Timestamp tMax) throws Exception{
+		
+		List<Timestamp> listSchedule = new ArrayList<Timestamp>();
+		
+		String timeMin = TimestampToRFC3399(tMin);
+		String timeMax = TimestampToRFC3399(tMax);
+		String url = "https://www.googleapis.com/calendar/v3/calendars/"
+				+ "osha1mpeirdg6m5uun9rmnbass@group.calendar.google.com/events?"
+				+ "key=AIzaSyDecA3yotvFE32SdVvzWqXHnH8AUAMkq68"
+				+ "&timeMin=" + timeMin
+				+ "&timeMax=" + timeMax;
+		
+		URLConnection conn = new URL(url).openConnection();
+		conn.setRequestProperty("Accept-Charset", "UTF-8");
+		InputStream response = conn.getInputStream();	
+		
+		Scanner s = new Scanner(response).useDelimiter("\\A");
+		String result = s.hasNext() ? s.next() : "";
+		
+		JsonObject jobj = new Gson().fromJson(result, JsonObject.class);
+		JsonArray ja = jobj.getAsJsonArray("items");
+		
+		for(int i=0; i<ja.size(); ++i){
+			JsonElement j = ja.get(i);
+			JsonObject jo = j.getAsJsonObject();
+			JsonElement start = jo.get("start");
+			JsonElement end   = jo.get("end");
+			String startStr = start.getAsJsonObject().get("dateTime").toString();
+			String endStr = end.getAsJsonObject().get("dateTime").toString();
+			startStr = startStr.substring(1, startStr.length()-1);
+			endStr = endStr.substring(1, endStr.length()-1);
+			listSchedule.add(RFC3399ToTimestamp(startStr));
+			listSchedule.add(RFC3399ToTimestamp(endStr));
+		}
+		
+		return listSchedule;
+	}
+	
 }
